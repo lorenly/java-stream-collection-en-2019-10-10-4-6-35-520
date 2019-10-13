c277dbfa35643143a33813547a8df25b90290b68
LORENLY <lyrol.lorenzo@oocl.com>
2019-10-13 23:38:58 +0800

    All Test Passed

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -3,7 +3,9 @@ package com.thoughtworks.collection;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyMap {
 
@@ -17,22 +19,36 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        return this.array.stream()
+                .map(num -> num * 3)
+                .collect(Collectors.toList());
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        return this.array.stream()
+                .map(num -> letterList.get(num - 1))
+                .collect(Collectors.toList());
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        return this.array.stream()
+                .map(num -> {
+                    --num;
+                    return num > 25 ? letterList.get(num / 26 - 1) + letterList.get(num % 26):
+                            letterList.get(num % 26);
+                })
+                .collect(Collectors.toList());
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        return this.array.stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        return this.array.stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
